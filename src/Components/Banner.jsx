bca695f5f8477cadeba4b6475787cc7ee066a5c6
abir781 <hasan.abir3176@gmail.com>
2025-07-02 14:59:25 +0600

    polishing2

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -21,18 +21,18 @@ const Banner = () => {
       scrollbar={{ draggable: true }}
     >
       <SwiperSlide className='text-white'>
-        <img className='h-[70vh] relative rounded-2xl' src="/slider1.jpg" alt="" style={{width:"100%"}} />
+        <img className='h-[70vh] relative ' src="/slider1.jpg" alt="" style={{width:"100%"}} />
         <h1 className='text-white text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>Stay Fresh, Stay Ahead</h1>
         
         </SwiperSlide>
       <SwiperSlide className='text-white'>
-        <img className='h-[70vh] relative rounded-2xl' src="/slider2.jpg" alt="" style={{width:"100%"}}/>
+        <img className='h-[70vh] relative ' src="/slider2.jpg" alt="" style={{width:"100%"}}/>
         <h1 className='text-white text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center '>Track Smart. Waste Less</h1>
         
 
         </SwiperSlide>
       <SwiperSlide className='text-white'>
-        <img className='h-[70vh] relative rounded-2xl' src="/slider3.jpg" alt="" style={{width:"100%"}}/>
+        <img className='h-[70vh] relative ' src="/slider3.jpg" alt="" style={{width:"100%"}}/>
          <h1 className='text-white text-6xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>Know What's in Your Fridge - Before It's Too Late.</h1>
         
         </SwiperSlide>
